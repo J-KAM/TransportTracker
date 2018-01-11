cfe2f4f76c5eab46b6fecc913ad5c80ea1f75c8f
Ana <1994.ana@gmail.com>
2018-01-11 19:29:03 +0100

    Added date rules and currency for created grammar. Created several files with valid and invalid use of the grammar. #1 #2

--- TransportTracker/generation/generator.py
+++ TransportTracker/generation/generator.py
@@ -4,10 +4,13 @@ from TransportTracker.execute.execute import execute
 from TransportTracker.settings import BASE_DIR
 
 
-def main(debug=False):
+def main(example_file_name,debug=False):
     path = BASE_DIR + "\TransportTracker"
-    model = execute(os.path.join(path, "grammar"), 'grammar.tx', 'example.grammar', debug, debug)
+    model = execute(os.path.join(path, "grammar"), 'grammar.tx', example_file_name, debug, debug)
 
 
 if __name__ == '__main__':
-    main(True)
+    main('example.grammar', True)
+    main('example1.grammar', True)
+    main('example2.grammar', True)
+    #main('example13.grammar', True)
