564de451382f4fd532ece083c3f441c95a201823
Jelena K <jelenakalabic@gmail.com>
2018-02-07 16:57:42 +0100

    Preparing user ticket query for interpretation and making model. #6

--- TransportTracker/execute/execute.py
+++ TransportTracker/execute/execute.py
@@ -19,9 +19,32 @@ def execute(path, grammar_file_name, example_file_name, export_dot, export_png):
 
     model_path = os.path.join(path, example_file_name)
     model_name = os.path.splitext(model_path)[0]
-
     model = metamodel.model_from_file(model_path)
 
+
+    if export_dot:
+        model_export(model, model_name + '.dot')
+    if export_png:
+        graph = pydot.graph_from_dot_file(model_name + '.dot')
+        graph[0].write_png(model_name + '.png')
+
+    return model
+
+
+def execute_for_web(path, grammar_file_name, query, export_dot, export_png):
+
+    meta_path = os.path.join(path, grammar_file_name)
+    meta_name = os.path.splitext(meta_path)[0]
+    metamodel = metamodel_from_file(meta_path)
+
+    if export_dot:
+        metamodel_export(metamodel, meta_name + '.dot')
+        if export_png:
+            graph = pydot.graph_from_dot_file(meta_name + '.dot')
+            graph[0].write_png(meta_name + '.png')
+
+    model = metamodel.model_from_str(query)
+    model_name = path + '/query'
 
     if export_dot:
         model_export(model, model_name + '.dot')
--- core/views.py
+++ core/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from django.views import View
+
+from core.forms import QueryForm
+
+from textx.exceptions import TextXSyntaxError
+import os
+from TransportTracker.settings import BASE_DIR
+from TransportTracker.execute.execute import execute_for_web
 
 
-def index(request):
-    return render(request, "core/index.html")
+class QueryFormView(View):
+    form_class = QueryForm
+    template_name = 'core/index.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form, 'error_message': ''})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        query = request.POST['query'].lower()
+        query = " ".join(query.splitlines())
+
+        path = ""
+        if BASE_DIR.__contains__('/'):
+            path = BASE_DIR + "/TransportTracker"  # linux
+        else:
+            path = BASE_DIR + "\TransportTracker"  # windows
+
+        try:
+            model = execute_for_web(os.path.join(path, "grammar"), 'grammar.tx', query, True, True)
+        except TextXSyntaxError:
+            return render(request, self.template_name, {'form': form, 'error_message': 'Syntax error'})
+
+        return render(request, self.template_name, {'form': form, 'error_message': ''})
