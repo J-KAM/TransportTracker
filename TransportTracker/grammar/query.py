48c8e686669af89cbb693eb311c7aa62f35a35c2
Ana <1994.ana@gmail.com>
2018-02-08 17:51:47 +0100

    Validation for price and currency as well as fro arrival and departure cities added to interpreter. #5

--- TransportTracker/grammar/query.py
+++ TransportTracker/grammar/query.py
@@ -7,11 +7,13 @@ class TicketQuery(object):
 
     def interpret(self, model):
         ticket_class_processor(model.transport_type.transport, model.ticket_class.Class)
+        cities_processor(model.From.departure_city, model.to.arrival_city)
         date_format_processor(model.on.departure_date)
         round_trip_processor(model.ticket_type.type, model.return_date)
         if model.return_date is not None:
             date_format_processor(model.return_date.return_date)
             date_processor(model.on.departure_date, model.return_date.return_date)
+        price_currency_processor(model.price, model.currency)
 
 
 def ticket_class_processor(transport_type, ticket_class):
@@ -19,6 +21,11 @@ def ticket_class_processor(transport_type, ticket_class):
         raise TextXSemanticError('Plane ticket class must be economy or business.')
     elif transport_type == 'train' and ticket_class != 'first' and ticket_class != 'second' and ticket_class != 'couchette':
         raise TextXSemanticError('Train ticket class must be first, second or couchette.')
+
+
+def cities_processor(departure_city, arrival_city):
+    if departure_city == arrival_city:
+        raise TextXSemanticError('Departure and arrival city cannot be the same.')
 
 
 def round_trip_processor(ticket_type, return_date):
@@ -42,3 +49,11 @@ def date_format_processor(date):
 def date_processor(departure_date, return_date):
     if return_date < departure_date:
         raise TextXSemanticError('Return date must be after departure.')
+
+
+def price_currency_processor(price, currency):
+    if price is not None and currency is None:
+        raise TextXSemanticError('You must specify currency for the maximum price.')
+    elif currency is not None and price is None:
+        raise TextXSemanticError('You must specify maximum price for the ticket.')
+
